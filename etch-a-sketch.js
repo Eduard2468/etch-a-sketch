1c76dadc6889aa089b1b7d47279cfd0cd4bd1e2b
eduard <eduardleon2468@gmail.com>
2022-11-12 05:14:11 -0800

    changed square size to 16x16 squares

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,7 @@ console.log("hello world")
 
 const container = document.querySelector('#container')
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
 square = document.createElement('div');
 square.classList.add('square')
 
