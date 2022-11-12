d0bc1b89910752f8572a08ca30ed3baaf066becd
eduard <eduardleon2468@gmail.com>
2022-11-11 16:07:09 -0800

    16 divs inside a container

---
+++ etch-a-sketch.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+
+const container = document.querySelector('#container')
+
+for (let i = 0; i < 16; i++) {
+square = document.createElement('div');
+square.classList.add('square')
+
+container.appendChild(square);
+}
+
